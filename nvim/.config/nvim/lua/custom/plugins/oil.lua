9a82a7892e138afd6e7b413ecde4260e29e6a8a8
Arkadiusz Żmudzin <arek@moodup.team>
2024-08-06 18:11:18 +0200

    show hidden files by default

--- nvim/.config/nvim/lua/custom/plugins/oil.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -4,7 +4,11 @@ return {
 	-- Optional dependencies
 	dependencies = { "echasnovski/mini.icons" },
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true
+			}
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 	end
 }
